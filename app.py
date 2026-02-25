468e4fa82b61287afd0f4843b616b11d65c2a6f2
Trieu Quoc Quan <trieuquanno1@trieuquan.com>
2026-02-26 00:06:12 +0700

    .

--- app.py
+++ app.py
@@ -107,15 +107,19 @@ class IdsConsoleApp:
             host_multi_output = FlowMultiModelOutput.from_proba(self.host_multi.predict_proba(host_multi_array)[0])
             flow_multi_output = FlowMultiModelOutput.from_proba(self.flow_multi.predict_proba(flow_multi_array)[0])
 
-        print(f"Stage 1 - Binary Outputs: {host_bin_output.label.value} / {flow_bin_output.label.value}")
-        print(f"host_bin_output: {host_bin_output.label.value}")
-        print(f"flow_bin_output: {flow_bin_output.label.value}")
+
         final_label = self.fusion.fuse(
             host_bin_output=host_bin_output,
             flow_bin_output=flow_bin_output,
             host_multi_output=host_multi_output,
             flow_multi_output=flow_multi_output
         )
+
+        if final_label.name.upper() != "BENIGN":
+            print(f"Stage 1 - Binary Outputs: {host_bin_output.label.name} / {flow_bin_output.label.name}")
+            print(f"host_bin_output: {host_bin_output.label.name}")
+            print(f"flow_bin_output: {flow_bin_output.label.name}")
+
 
         confidence = max(
             host_bin_output.confidence,
